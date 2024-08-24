999da8757ada6105f96f5aab514c2c6b6594a765
Vishv0407 <vishvkumar.b@ahduni.edu.in>
2024-08-25 00:03:38 +0530

    added logout

--- src/Components/core/Navbar/ProfileDropdown.jsx
+++ src/Components/core/Navbar/ProfileDropdown.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { logout } from '../../../Services/Opertations/authAPI';
+import toast from 'react-hot-toast';
 
 function ProfileDropdown() {
   const dispatch = useDispatch();
@@ -31,8 +32,29 @@ function ProfileDropdown() {
   }, [isOpen]);
 
   const logoutAction = () => {
-    dispatch(logout(navigate));
-  }
+    toast((t) => (
+      <div>
+        <p className='pb-4 pt-2'>Are you sure you want to logout?</p>
+        <div className="flex justify-between">
+          <button
+            className="hover:bg-richblack-200 border-[1px] border-richblack-400 text-black font-bold py-2 px-4 rounded"
+            onClick={() => toast.dismiss(t.id)}
+          >
+            Cancel
+          </button>
+          <button
+            className="bg-[#ff000030] hover:bg-[#ff000050] border-[1px] border-[#ff000040] text-[#ff0000] font-bold py-2 px-4 rounded"
+            onClick={() => {
+              dispatch(logout(navigate));
+              toast.dismiss(t.id);
+            }}
+          >
+            Logout
+          </button>
+        </div>
+      </div>
+    ));
+  };
 
   return (
     <div className="flex items-center relative">
@@ -41,12 +63,12 @@ function ProfileDropdown() {
       </button>
       {isOpen && (
         <ul className="absolute bg-white/10 text-white border-[1px] border-richblack-300 backdrop-blur-sm p-4 top-[100%] translate-y-3 left-[-75%] flex gap-2 flex-col rounded-lg">
-          <li >
+          <li>
             <a href="/profile">Profile</a>
           </li>
           <hr />
           <li>
-            <a onClick={logoutAction} className='cursor-pointer'>Logout</a>
+            <a onClick={logoutAction} className="cursor-pointer">Logout</a>
           </li>
         </ul>
       )}
